d49b2ee8145f7eef8e5025964847f0274072623f
Patrick McDevitt <pair+pmcde@allstate.com>
2017-05-24 16:55:49 +0100

    Production Build

---
+++ src/api/baseUrl.js
@@ -0,0 +1,13 @@
+export const getBaseUrl = () => {
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/'
+}
+
+const getQueryStringParameterByName = (name, url) => {
+  if (!url) url = window.location.href
+  name = name.replace(/[\[\]]/g, "\\$&")
+  let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+  results = regex.exec(url)
+  if(!results) return null
+  if (!results[2]) return ''
+  return decodeURIComponent(results[2].replace(/\+/g, " "))
+}
